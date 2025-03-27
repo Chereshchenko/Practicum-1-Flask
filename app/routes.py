761b9569e332d040aebdda447f12e55d843836d4
mouse <arina021104@gmail.com>
2025-03-27 21:28:58 +0300

    lesson 9

--- app/routes.py
+++ app/routes.py
@@ -1,19 +1,40 @@
 from pyexpat.errors import messages
 
 from flask import render_template, request, redirect, url_for, flash
+from datetime import datetime
+
 from app import app
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    current_time = datetime.now()
+    formatted_time = current_time.strftime("%A, %B %d, %Y, %H:%M:%S")
+    return render_template("index.html", current_time=formatted_time)
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    team_members = [
+
+        {'name': 'Ариша', 'role': 'Разработчик'},
+
+        {'name': 'Ашка', 'role': 'Дизайнер'},
+
+        {'name': 'Крысь', 'role': 'Менеджер проекта'}
+
+    ]
+    return render_template('about.html', team_list=team_members)
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    contact_info = {
+        'name': 'Ариша',
+        'address': {
+            'street': 'улица 1-го мая',
+            'city': 'Смоленск',
+            'zip': '43242'
+        }
+    }
+    return render_template('contact.html', info=contact_info)
 
 @app.route("/submit", methods=["POST", "GET"])
 def submit():
